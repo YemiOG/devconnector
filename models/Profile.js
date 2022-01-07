27468d7e341ec8664e52f7e89f5aa1cffc899009
Opeyemi Ogundeyi <yemi.ogundeyi@gmail.com>
2022-01-07 17:42:10 +0100

    profile

---
+++ models/Profile.js
@@ -0,0 +1,121 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+// create schema
+const ProfileSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users",
+  },
+  handle: {
+    type: String,
+    required: true,
+    max: 40,
+  },
+  website: {
+    type: String,
+  },
+  company: {
+    type: String,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  skills: {
+    type: [String],
+    required: true,
+  },
+  bio: {
+    type: String,
+  },
+  githubusername: {
+    type: String,
+  },
+  experience: [
+    {
+      titile: {
+        type: String,
+        required: true,
+      },
+      company: {
+        type: String,
+        required: true,
+      },
+      location: {
+        type: String,
+        required: true,
+      },
+      from: {
+        type: Date,
+        required: true,
+      },
+      to: {
+        type: Date,
+      },
+      current: {
+        type: Boolean,
+        required: false,
+      },
+      description: {
+        type: String,
+      },
+    },
+  ],
+  education: [
+    {
+      school: {
+        type: String,
+        required: true,
+      },
+      degree: {
+        type: String,
+        required: true,
+      },
+      fieldofstudy: {
+        type: String,
+        required: true,
+      },
+      from: {
+        type: Date,
+        required: true,
+      },
+      to: {
+        type: Date,
+      },
+      current: {
+        type: Boolean,
+        required: false,
+      },
+      description: {
+        type: String,
+      },
+    },
+  ],
+  social: {
+    youtube: {
+      type: String,
+    },
+    facebook: {
+      type: String,
+    },
+    twitter: {
+      type: String,
+    },
+
+    linkedin: {
+      type: String,
+    },
+    instagram: {
+      type: String,
+    },
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Profile = mongoose.model("profile", ProfileSchema);
+
+export default Profile;
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,7 +1,103 @@
 import express from "express";
+import mongoose from "mongoose";
+import passport from "passport";
+
+// load Profile Model
+import Profile from "../../models/Profile.js";
+
+//load user profile
+import User from "../../models/User.js";
+
+//load validation
+import validateProfileInput from "../../validation/profile.js";
 
 const router = express.Router();
 //tests profile route - Public
-router.get('/test', (req,res)=> res.json({msg: 'Profile Works'}));
+router.get("/test", (req, res) => res.json({ msg: "Profile Works" }));
+
+// @route GET api/profile
+// @desc Get current users profile
+// @access Private
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res, _next) => {
+    const errors = {};
+    Profile.findOne({ user: req.user.id })
+      .then((profile) => {
+        if (!profile) {
+          errors.noprofile = "There is no profile for this User";
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch((err) => res.status(404).json(err));
+  }
+);
+
+// @route POST api/profile
+// @desc create users profile
+// @access Private
+
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res, _next) => {
+    const { errors, isValid } = validateProfileInput(req.body);
+
+    //check validation
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    //get fields
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (req.body.handle) profileFields.handle = req.body.handle;
+    if (req.body.company) profileFields.company = req.body.company;
+    if (req.body.location) profileFields.location = req.body.location;
+    if (req.body.status) profileFields.status = req.body.status;
+    if (req.body.website) profileFields.website = req.body.website;
+    //skills - split into array
+    if (typeof req.body.skills !== "undefined")
+      profileFields.skills = req.body.skills.split(",");
+    if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.githubusername)
+      profileFields.githubusername = req.body.githubusername;
+
+    //social
+    profileFields.social = {};
+    if (req.body.youtube) profileFields.youtube = req.body.youtube;
+    if (req.body.twitter) profileFields.twitter = req.body.twitter;
+    if (req.body.facebook) profileFields.facebook = req.body.facebook;
+    if (req.body.instagram) profileFields.instagram = req.body.instagram;
+    if (req.body.linkedin) profileFields.linkedin = req.body.linkedin;
+
+    Profile.findOne({ user: req.user.id }).then((profile) => {
+      //update profile
+      if (profile) {
+        Profile.findByIdAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        ).then((profile) => res.json(profile));
+      } else {
+        // create profile
+
+        // check if handle exists
+        Profile.findOne({ handle: profileFields.handle }).then((profile) => {
+          if (profile) {
+            errors.handle = "The handle already exists";
+            res.status(400).json(errors);
+          }
+          // save profile
+          new Profile(profileFields)
+            .save()
+            .then((profile) => res.json(profile));
+        });
+      }
+    });
+  }
+);
 
 export default router;
