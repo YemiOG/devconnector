16c304194eb24ead1a8ff9ad74159e1d4c7559ce
Opeyemi Ogundeyi <yemi.ogundeyi@gmail.com>
2022-01-16 23:04:26 +0100

    j

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -86,27 +86,31 @@ router.delete(
   }
 );
 
-// @route POST api/like/:id
-// @desc  Delete
+// @route POST api/posts/like/:id
+// @desc  Like post
 // @access Private
 router.post(
-  "like/:id",
+  "/like/:id",
   passport.authenticate("jwt", { session: false }),
   (req, res, _next) => {
     Profile.findOne({ user: req.user.id }).then((profile) => {
       Post.findById(req.params.id)
         .then((post) => {
-          //check for post owner
-          if (post.user.toString() !== req.user.id) {
+          if (
+            post.likes.filter((like) => like.user.toString() === req.user.id)
+              .length > 0
+          ) {
             return res
-              .status(401)
-              .json({ notauhorized: "User not authorized" });
+              .status(400)
+              .json({ alreadyliked: "User already like this post" });
           }
-          //Delete
-          post.remove().then(() => res.json({ sucess: true }));
+
+          //Add user id to likes array
+          post.likes.unshift({ user: req.user.id });
+          post.save().then();
         })
         .catch((err) =>
-          res.status(404).json({ postnotfound: "No post found" })
+          res.status(404).json({ postnotfound: "No Post found" })
         );
     });
   }
