019aac5efc77108266e3c37290b9f0164a2af942
Opeyemi Ogundeyi <yemi.ogundeyi@gmail.com>
2022-01-22 22:34:56 +0100

    landing

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
+import Navbar from "./component/layout/Navbar";
+import Footer from "./component/layout/Footer";
+import Landing from "./component/layout/Landing";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>My ReactApp</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <Landing />
+        <Footer />
+      </div>
+    );
+  }
 }
-
 export default App;
