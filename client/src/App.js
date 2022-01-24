89bda00169ad56b95ac33409e1a4d403be03dc7b
Opeyemi Ogundeyi <yemi.ogundeyi@gmail.com>
2022-01-24 22:54:19 +0100

    App

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,27 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./component/layout/Navbar";
 import Footer from "./component/layout/Footer";
 import Landing from "./component/layout/Landing";
 import "./App.css";
+import Register from "./component/auth/Register";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Landing />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path = "/register" component = {Register}/>
+            <Route exact path = "/login" component = {Login}/>
+          </div>
+
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
